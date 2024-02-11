5c8d59e8403abe20984f7bef77a082503c855697
ShubhamKr-047 <131558217+ShubhamKr-047@users.noreply.github.com>
2024-02-11 14:24:36 +0530

    Add files via upload

---
+++ Policy_Based_Data_Structure.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+// for pbds :- s.insert(), s.erase(), *s.find_by_order(x) -> works as index, s.order_of_key(x) -> number of element less than x,
+// for descending order use greater<int> and for multiset use less_equal<int> and for different datatypes replace int with it
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    solve();
+}
