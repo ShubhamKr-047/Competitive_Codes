8d30b82809c0939917fbabb1e8efa3b0a8d6607b
ShubhamKr-047 <131558217+ShubhamKr-047@users.noreply.github.com>
2024-07-17 00:42:33 +0530

    Add files via upload

--- Maths/prime_factors.cpp
+++ Maths/prime_factors.cpp
@@ -1,50 +1,26 @@
-/*
-
-ans matrix stores the final result (TC :- dim^3 * log(pow))
-to calculate nth fibonacci number
-take 1st matrix as [0 1] and 2nd matrix ans |0 1|
-                                            |1 1|
-
-[F1 F2] * |0 1|^(n - 1) = [Fn Fn+1]
-          |1 1|
-
-*/
-
-vector<vector<int>> matrix_multiplication(int &dim, vector<vector<int>> &a, vector<vector<int>> &b)
+vector<int> prime_factor(int n) 
 {
-    vector<vector<int>> mul(dim, vector<int> (dim, 0));
-    for (int i = 0; i < dim; i++) 
-    {
-        for (int j = 0; j < dim; j++) 
-        {
-            for (int k = 0; k < dim; k++) 
-            {
-                mul[i][j] += a[i][k] * b[k][j];
-            }
+    vector<int> prime_factors; 
+    for (int i = 2; i * i <= n; i++) 
+    { 
+        if (n % i == 0) prime_factors.push_back(i);
+        while (n % i == 0) n /= i;
+    }
+    if (n > 2) prime_factors.push_back(n);
+    return prime_factors; 
+}
+
+unordered_map<int, int> prime_factor_count(int n)
+{
+    unordered_map<int, int> prime_factors; 
+    for (int i = 2; i * i <= n; i++) 
+    { 
+        while (n % i == 0) 
+        { 
+            prime_factors[i]++; 
+            n /= i;
         }
     }
-    return mul;
-}
-
-void matrix_exponentiation(int &dim, int &pw, vector<vector<int>> &mat, vector<vector<int>> &ans)
-{
-    int i = 0, j = 0;
-    while (i < dim) ans[i][j] = 1, i++, j++;
-    while (pw) 
-    {
-        if (pw & 1LL) ans = matrix_multiplication(dim, ans, mat);
-        mat = matrix_multiplication(dim, mat, mat);;
-        pw >>= 1LL;
-    }
-}
-
-void solve()
-{
-    int dim, pw;
-    cin >> dim >> pw;
-    vector<vector<int>> mat(dim, vector<int> (dim));
-    vector<vector<int>> ans(dim, vector<int> (dim, 0));
-    for (auto &it : mat) cin >> it;
-    matrix_exponentiation(dim, pw, mat, ans);
-    for (auto &it : ans) cout << it;
+    if (n > 1) prime_factors[n]++;
+    return prime_factors; 
 }
