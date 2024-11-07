f2309b39ac05e4479d680f436d315b9711d6d0ca
Shubham Kumar <131558217+ShubhamKr-047@users.noreply.github.com>
2024-11-08 01:08:42 +0530

    Update Sparse_Table.cpp

--- Range Based Data Structure/Sparse_Table.cpp
+++ Range Based Data Structure/Sparse_Table.cpp
@@ -11,10 +11,7 @@ public:
     {
         n = arr.size();
         lg.resize(n + 1, 0);
-        for (int i = 2; i <= n; i++)
-        {
-            lg[i] = lg[i / 2] + 1;
-        }
+        for (int i = 2; i <= n; i++) lg[i] = lg[i / 2] + 1;
         st.resize(n, vector<int>(lg[n] + 1));
         for (int i = 0; i < n; i++) st[i][0] = arr[i];
         for (int j = 1; j <= lg[n]; j++)
