52db052e11187e92cf3235f5ce0625bf39b6f0a9
Shubham Kumar <131558217+ShubhamKr-047@users.noreply.github.com>
2024-11-14 17:57:54 +0530

    Update sieve_algorithm.cpp

--- Maths/sieve_algorithm.cpp
+++ Maths/sieve_algorithm.cpp
@@ -1,6 +1,7 @@
 vector<bool> SieveOfEratosthenes(int n)
 {
     vector<bool> prime(n + 1, true);
+    prime[0] = prime[1] = false;
     for (int p = 2; p * p <= n; p++) 
     {
         if (prime[p] == true) 
