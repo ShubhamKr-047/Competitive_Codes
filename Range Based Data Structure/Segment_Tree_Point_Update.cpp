0ba4ec797c8429ede1663dad4c48fbb7907b9c2a
Shubham Kumar <131558217+ShubhamKr-047@users.noreply.github.com>
2026-03-03 13:57:43 +0530

    Refactor Segment Tree for point updates

--- Range Based Data Structure/Segment_Tree_Point_Update.cpp
+++ Range Based Data Structure/Segment_Tree_Point_Update.cpp
@@ -1,15 +1,12 @@
 //This is initially for 0 - based indexing
 // For one based indexing just make the initial vector 1 - based indexing
+// SGTree st(v) -> This builds the sg tree where v is the vector on which we have to use sg tree
 
 class SGTree
 {
-
 public:
-
     int n;
     vector<int> seg;
-
-    //building the SG Tree
     SGTree(vector<int> &arr)
     {
         n = sz(arr);
@@ -19,11 +16,9 @@ public:
 
     void build(vector<int> &arr) { build(0, 0, n - 1, arr); }
     int query(int l, int r) { return query(0, 0, n - 1, l, r); }
-    void update(int i, int val) { update(0, 0, n - 1, i, val); }
+    void update(int ind, int val) { update(0, 0, n - 1, ind, val); }
 
 private:
-
-    //sg.build(0, 0, n - 1, arr);
     void build(int ind, int low, int high, vector<int> &arr)
     {
         if (low == high)
@@ -37,12 +32,9 @@ private:
         seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);
     }
 
-    //sg.query(0, 0, n - 1, l, r);
     int query(int ind, int low, int high, int l, int r)
     {
-        // no overlap
         if (r < low || high < l) return INT_MAX;
-        // complete overlap
         if (low >= l && high <= r) return seg[ind];
         int mid = (low + high) >> 1;
         int left = query(2 * ind + 1, low, mid, l, r);
@@ -50,7 +42,6 @@ private:
         return min(left, right);
     }
 
-    //sg.update(0, 0, n - 1, index, value);
     void update(int ind, int low, int high, int i, int val)
     {
         if (low == high)
@@ -64,3 +55,4 @@ private:
         seg[ind] = min(seg[2 * ind + 1], seg[2 * ind + 2]);
     }
 };
+
